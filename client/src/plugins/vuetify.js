86010fa9b05d48e2f91ca3530c72faca790f41ed
Văn Viết Hiếu Anh <vanviethieuanh@gmail.com>
2021-03-30 02:35:58 +0700

    💬 Add about dialog, 🌼 Change icon to Font Awesome

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,11 +1,12 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import 'font-awesome/css/font-awesome.min.css'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
     icons: {
-        iconfont: 'mdi', // default - only for display purposes
-    },
-});
+        iconfont: 'fa4' // default - only for display purposes
+    }
+})
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,38 +4,137 @@
             <router-link tag="button" to="/create">Create</router-link>
             <router-link tag="button" to="/select">Join</router-link>
         </div>
+
+        <div class="text-center">
+            <v-dialog width="500">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn icon color="blue" v-on="on" v-bind="attrs">
+                        <v-icon>fa4 fa-info</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-card>
+                    <v-card-text>
+                        <div
+                            class="text--primary text-left text-subtitle-1 pt-6"
+                        >
+                            👋 Hi! This is a real-time group chat application
+                            implemented using Node.js + Socket.IO and + VueJS.
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn color="blue" @click="show = !show" text>
+                            function
+                        </v-btn>
+
+                        <v-btn icon @click="show = !show">
+                            <v-icon>{{
+                                show ? 'mdi-chevron-up' : 'mdi-chevron-down'
+                            }}</v-icon>
+                        </v-btn>
+                    </v-card-actions>
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <v-divider></v-divider>
+
+                            <v-card-text
+                                class="text--primary text-left text-subtitle-1 pt-6"
+                            >
+                                💬 Create chat room (have passcode or not).
+                                <br />🚪 Browse room in real-time for joining.
+                                <br />🔗 Share room url for joining
+                            </v-card-text>
+                        </div>
+                    </v-expand-transition>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn
+                                    icon
+                                    href="https://github.com/vanviethieuanh/group-chat"
+                                    target="_blank"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                >
+                                    <v-icon>fa4 fa-github</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Repository</span>
+                        </v-tooltip>
+
+                        <v-tooltip top>
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn
+                                    icon
+                                    href="https://vanviethieuanh.com"
+                                    target="_blank"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                >
+                                    <v-icon>fa4 fa-globe</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>Websites</span>
+                        </v-tooltip>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Home',
-    components: {}
+    components: {},
+    data() {
+        return { show: false }
+    },
+    watch: {
+        dialog(val) {
+            if (!val) return
+
+            setTimeout(() => (this.dialog = false), 4000)
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../variables.scss';
 
-#nav {
-    padding: 0;
-    height: 100vh;
+.home {
+    .text-center {
+        position: fixed;
+        z-index: 100;
+        right: 20px;
+        top: 20px;
+    }
 
-    display: flex;
+    #nav {
+        padding: 0;
+        height: 100vh;
 
-    button {
-        width: 100%;
-        font-size: 70px;
+        display: flex;
 
-        transition: $fast-transition;
+        button {
+            width: 100%;
+            font-size: 70px;
 
-        &:hover {
-            width: $expand-column-width;
-        }
+            transition: $fast-transition;
 
-        &:nth-child(2n + 1) {
-            background: $main-color;
-            color: white;
+            &:hover {
+                width: $expand-column-width;
+            }
+
+            &:nth-child(2n + 1) {
+                background: $main-color;
+                color: white;
+            }
         }
     }
 }
